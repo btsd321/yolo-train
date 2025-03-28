48da3e99fa50964aed4e6f65fe5dc3e0681922e5
btsd321 <1123347532@qq.com>
2025-03-28 12:07:48 +0800

    feat: 增加pose模型支持与缓存文件忽略

--- train.py
+++ train.py
@@ -4,12 +4,13 @@ import datetime
 import random
 import torch
 import sys
+import onnx
 
 if __name__ == '__main__':
     # Load a model
-    model = YOLO("yolo11n-seg.yaml")  # build a new model from YAML
-    model = YOLO("yolo11n-seg.pt")  # load a pretrained model (recommended for training)
-    model = YOLO("yolo11n-seg.yaml").load("yolo11n.pt")  # build from YAML and transfer weights
+    # model = YOLO("yolo11n-seg.yaml")  # build a new model from YAML
+    # model = YOLO("yolo11n-seg.pt")  # load a pretrained model (recommended for training)
+    model = YOLO("yolo11n-seg.yaml").load("yolo11n-seg.pt")  # build from YAML and transfer weights
 
     print(torch.version.cuda)# 查看cuda版本
     # Train the model
@@ -33,10 +34,23 @@ if __name__ == '__main__':
         )
         
     # 对单张图片进行推理并可视化结果
-    results = model.predict("D:\\Project\\RobotMaster\\yolo-train\\dataset\\images\\test\\20250326_132435_7GKAFKiu.jpg", visualize=True)
+    results = model(["D:\\Project\\RobotMaster\\yolo-train\\dataset\\images\\test\\20250326_132435_7GKAFKiu.jpg"])  # return a list of Results objects
 
-    # 显示图片
-    print(results)
+    # Process results list
+    for result in results:
+        boxes = result.boxes  # Boxes object for bounding box outputs
+        masks = result.masks  # Masks object for segmentation masks outputs
+        keypoints = result.keypoints  # Keypoints object for pose outputs
+        probs = result.probs  # Probs object for classification outputs
+        obb = result.obb  # Oriented boxes object for OBB outputs
+        # result.show()  # display to screen
+        result.save(filename="result.jpg")  # save to disk
+        # 打印 boxes, masks, keypoints, probs, obb 信息
+        print("boxes: ", boxes)
+        print("masks: ", masks)
+        print("keypoints: ", keypoints)
+        print("probs: ", probs)
+        print("obb: ", obb)
 
     model.eval()
 
